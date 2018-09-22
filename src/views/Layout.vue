bdc7794ad40128fe98df5e5d6a35067aeed8e7db
胡玮文 <huww98@outlook.com>
2018-09-22 23:28:53 +0800

    后台管理框架

---
+++ src/views/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="dashboard">仪表盘</el-menu-item>
+      <el-menu-item index="goods">物品</el-menu-item>
+      <el-menu-item index="logout" class="right-menu-item">注销</el-menu-item>
+    </el-menu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    activeIndex() {
+      return this.$route.name;
+    }
+  },
+  methods: {
+    handleSelect(key) {
+      if (key === "logout") {
+        // do logout
+        this.$router.replace({ name: "login" });
+      } else {
+        this.$router.replace({ name: key });
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.right-menu-item {
+  float: right !important;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login-container">
+    <el-card class="login-card" shadow="always">
+      <h2 slot="header" class="login-title">
+        华工二手市场后端
+      </h2>
+      <el-form :model="loginForm" label-width="60px" @submit.native.prevent="login">
+        <el-form-item label="用户名">
+          <el-input v-model="loginForm.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-button native-type="submit" type="primary" class="login-button">登录</el-button>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {}
+    };
+  },
+  methods: {
+    login() {
+      console.log("login");
+      this.$router.replace({name: 'dashboard'});
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.login {
+  &-title {
+    text-align: center;
+  }
+
+  &-container {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    background: gray;
+    min-height: 100vh;
+  }
+
+  &-card {
+    width: 320px;
+    margin-top: 6em;
+  }
+  &-button {
+    width: 100%;
+  }
+}
+</style>
